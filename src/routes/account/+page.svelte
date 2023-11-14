0d01f2c81e1974cea95015970a2d590c621ae2b4
Zachary Cheng <zach.ch02@gmail.com>
2023-11-14 09:27:16 +0800

    first commit

---
+++ src/routes/account/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let data;
+
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
+</script>
+
+<div class="flex flex-col items-center">
+	<h1>Account</h1>
+	{#if session}
+		<p>Logged in as {session.user.email}</p>
+	{:else}
+		<p>Not logged in</p>
+	{/if}
+</div>
